01530bc90eebe07d5cc10b85ea914785dba5ec63
StevenWorrall <steven.worrall@gmail.com>
2018-07-24 18:48:10 -0700

    added assets

---
+++ contactsApp/contactsApp/QRView/QRViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QRViewController.swift
+//  contactsApp
+//
+//  Created by Steven Worrall on 7/24/18.
+//  Copyright © 2018 Enthusio. All rights reserved.
+//
+
+import UIKit
+
+class QRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
