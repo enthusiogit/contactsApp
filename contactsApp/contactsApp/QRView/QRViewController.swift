3c34ba4ff5dd0d814dc0f15f689799b303e43d4a
StevenWorrall <steven.worrall@gmail.com>
2018-07-24 19:42:00 -0700

    just some stuff

--- contactsApp/contactsApp/QRView/QRViewController.swift
+++ contactsApp/contactsApp/QRView/QRViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class QRViewController: UIViewController {
+    var imgURL: UIImage?
 
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var qrImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        qrImage.image = imgURL
+        
+        closeButton.layer.borderWidth = 1
+        closeButton.layer.borderColor = UIColor.black.cgColor
     }
-
+    
+    @IBAction func closePress(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+    }
 }
